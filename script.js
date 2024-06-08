1283b15e94151c254ae4a48f228f3808c6340b0c
Deepika A M <131507410+DeepikaAM02@users.noreply.github.com>
2024-06-08 21:11:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+let isExplore1 = false;
+
+function region1() { 
+    if (!isExplore1) {
+        document.getElementById("descibes1").style.display = "block";
+        isExplore1 = true;
+    }
+    else if(isExplore1){ 
+        document.getElementById("descibes1").style.display = "none";
+        isExplore1 = false;
+    }
+    console.log("Hello");
+    
+    
+    
+}
+
+let isExplore2 = false;
+
+function region2() { 
+    if (!isExplore2) {
+        document.getElementById("descibes2").style.display = "block";
+        isExplore2 = true;
+    }
+    else if(isExplore2){ 
+        document.getElementById("descibes2").style.display = "none";
+        isExplore2 = false;
+    }
+    console.log("Hello");
+    
+    
+    
+}
+let isExplore3 = false;
+
+function region3() { 
+    if (!isExplore3) {
+        document.getElementById("descibes3").style.display = "block";
+        isExplore3 = true;
+    }
+    else if(isExplore3){ 
+        document.getElementById("descibes3").style.display = "none";
+        isExplore3 = false;
+    }
+    console.log("Hello");
+    
+    
+    
+}
+let isExplore4 = false;
+
+function region4() { 
+    if (!isExplore4) {
+        document.getElementById("descibes4").style.display = "block";
+        isExplore4 = true;
+    }
+    else if(isExplore4){ 
+        document.getElementById("descibes4").style.display = "none";
+        isExplore4 = false;
+    }
+    console.log("Hello");
+    
+    
+    
+}
+
+function bookNow() { 
+    alert("Successfully Booked");
+}
